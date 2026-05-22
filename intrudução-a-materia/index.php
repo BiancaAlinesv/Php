75f5b9b3c2bc19c27e411bf247ee64d48e3aead8
Bianca <bianca.alinedev@gmail.com>
2026-05-22 13:24:29 -0300

    featintrodução curso Php

---
+++ intrudução-a-materia/index.php
@@ -0,0 +1,27 @@
+<?php 
+
+//$nome = "Bianca";
+//$idade =29;
+//$altura = 1.82;
+
+//echo $idade + $altura;
+
+//$usuario = array(
+//  "Bianca",
+//30,
+//1.62,
+//);
+
+//for
+//($i=0; $i <= 10; $i++){
+//  echo $i. "<br/>";
+//}
+
+$nome = $_POST["nome"];
+$texto = $_POST["text"];
+$sexo = $_POST["Sexo"];
+
+echo $nome. "<br/>";
+echo $texto. "<br/>";
+echo $sexo. "<br/>";
+?>
