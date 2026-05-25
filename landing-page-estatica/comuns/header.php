b7305e79c4bd7b3e8a215cdd69229518e259f122
Bianca <bianca.alinedev@gmail.com>
2026-05-25 16:19:52 -0300

    featheader

---
+++ landing-page-estatica/comuns/header.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>All In - Treinamentos</title>
+    <base href="http://localhost/php/landing-page-estatica/">
+    <link rel="stylesheet" href="recursos/css/main.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/7.0.1/css/all.min.css" />
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@300; 400;700&display=swap" rel="stylesheet">
+
+<body>
+    <header>
+        <div class="container">
+            <div class="conteudo">
+                <div class="logo">
+                    <img src="recursos/imagens/logo.png" alt="All In - Treinamentos">
+                </div>
+                <div class="menu-lateral">
+                    <ul class="nav">
+                        <li>
+                            <a href="">Quem sou eu</a>
+                        </li>
+
+                        <li>
+                            <a href="#">Eventos</a>
+                        </li>
+
+                        <li>
+                            <a href="#">Cursos</a>
+                        </li>
+
+                        <li>
+                            <a href="#">Vídeos</a>
+                        </li>
+
+                        <li>
+                            <a href="#">Fale comigo</a>
+                        </li>
+                    </ul>
+
+                    <div class="redes-sociais">
+                        <p>Siga-me:</p>
+                        <a href=""><i class="fa-brands fa-facebook-f"></i></a>
+                        <a href=""><i class="fa-brands fa-instagram"></i></a>
+                        <a href=""><i class="fa-brands fa-youtube"></i></a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </header>
