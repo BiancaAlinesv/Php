518f345d8c4d6b87b624d900e3e6af3b4d1f9397
Bianca <bianca.alinedev@gmail.com>
2026-05-29 18:46:21 -0300

    feathome

--- landing-page-estatica/views/home.php
+++ landing-page-estatica/views/home.php
@@ -1,3 +1,19 @@
 <main>
-    
+    <section class="quem-sou-eu">
+        <div class="container">
+            <div class="conteudo">
+                <div class="img">
+                <img src="recursos/imagens/pessoa-banner.png" alt="">
+            </div>
+            <div class="texto"> 
+                <h2>Quem sou eu</h2>
+                <div class="descricao">
+                    <p>Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites</p>
+                    <p>There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text.</p>
+                </div>
+                <a class="btn" href="#">Veja o video</a>
+            </div>
+            </div>
+        </div>
+    </section>
 </main>
